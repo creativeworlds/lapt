604c452dbece2c71d136d819db1ba8f2e313ea71
Pramod Singh <spramodgusain@gmail.com>
2025-10-22 15:30:59 +0530

    chore: create model and migration table for invoice

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use App\Enums\InvoiceType;
 use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+    protected $casts = [
+        'invoice_type' => InvoiceType::class,
+        'amount' => 'float',
+        'discount' => 'float',
+        'fee_after_discount' => 'float',
+        'tax' => 'float',
+        'final_amount' => 'float',
+        'due_date' => 'date',
+    ];
+
+    protected $fillable = [
+        'invoice_type',
+        'centre_id',
+        'course_id',
+        'amount',
+        'discount',
+        'fee_after_discount',
+        'currency',
+        'tax',
+        'final_amount',
+        'quantity',
+        'due_date',
+        'emi',
+        'notes',
+    ];
 }
--- database/migrations/2025_10_14_095213_create_invoices_table.php
+++ database/migrations/2025_10_14_095213_create_invoices_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,19 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
-            
+            $table->enum('invoice_type', ['student', 'accreditation']);
+            $table->foreignId('centre_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('course_id')->constrained()->cascadeOnDelete();
+            $table->decimal('amount', 10, 2);
+            $table->decimal('discount', 5, 2)->default(0.00);
+            $table->decimal('fee_after_discount', 10, 2);
+            $table->string('currency')->nullable();
+            $table->decimal('tax', 5, 2)->default(0.00);
+            $table->decimal('final_amount', 10, 2)->default(0.00);
+            $table->integer('quantity')->default(1);
+            $table->date('due_date');
+            $table->string('emi', 1)->nullable();
+            $table->string('notes')->nullable();
             $table->timestamps();
         });
     }
