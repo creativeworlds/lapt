870b395e10800c639c811c75bf93f35d20f5c1f1
Pramod Singh <spramodgusain@gmail.com>
2025-10-23 14:56:39 +0530

    chore: create model seeder and migration table for countries

---
+++ app/Models/Country.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $fillable = [
+        'name',
+        'iso_code_2',
+        'iso_code_3',
+        'address_format',
+        'postcode',
+        'status',
+    ];
+}
---
+++ database/migrations/2025_10_23_084421_create_countries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('iso_code_2', 2);
+            $table->string('iso_code_3', 3);
+            $table->mediumText('address_format')->nullable();
+            $table->tinyInteger('postcode')->default(0);
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
