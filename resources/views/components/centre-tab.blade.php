db63dba21d22e8da68f86458c2b7dd71833e2733
Pramod Singh <spramodgusain@gmail.com>
2025-10-23 10:29:18 +0530

    chore: create centre tab component

---
+++ resources/views/components/centre-tab.blade.php
@@ -0,0 +1,24 @@
+<div id="header">
+    <div id="subnav">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="nav nav-pills">
+                        <li role="presentation">
+                            <a href="{{ route('centre-categories.create') }}" title="Add Category">Add Category</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="{{ route('centre-categories.index') }}" title="Edit Category">Edit Category</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="{{ route('centres.create') }}" title="Add centre">Add centre</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="{{ route('centres.index') }}" title="Edit centre">Edit centre</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
